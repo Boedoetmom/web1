323871ab847c108ce86c8f88c371521171d873e9
Boedoetmom <trimom806@gmail.com>
2022-09-23 21:48:53 +0700

    update model relation hasMany and hasOne

---
+++ app/Models/Operational/Transaction.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Operational;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+
+    public $table = 'transaction';
+    
+    protected $date = [
+    
+        'create_at',
+        'update_at',
+        'delete_at',
+    ];
+    
+    protected $fillable = [
+        'appointment_id',
+        'fee_doctor',
+        'fee_specialist',
+        'fee_hospital',
+        'sub_total',
+        'vat',
+        'total',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    function appointment() {
+
+        return $this->belongsTo('App\Models\Operational\Appointment', 'Appointment_id', 'id');
+    }
+}
